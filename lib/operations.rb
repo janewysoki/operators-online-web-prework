8bed44ec2a310a2d96ddd7229fb426fa97283e44
Jane Wysoki <jane.wysoki@gmail.com>
2019-06-13 17:30:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
   if speed > 60
     return true
-  else speed < 40
-    return true
-  elsif speed > 40 && < 60
+  elsif speed > 40 && speed < 60
     return false
+  else
+    return true
   end
 end
 
